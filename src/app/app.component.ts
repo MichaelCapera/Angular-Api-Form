7053ca551339912652f54595a04db76ca955cab1
Michael Capera <michaelcapera@gmail.com>
2021-09-26 18:07:06 -0500

    main index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonalService } from './services/personal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontPersonal';
+export class AppComponent implements OnInit {
+
+
+  constructor(private personalService:PersonalService){}
+  
+  ngOnInit(){
+
+    this.personalService.getPersonal().subscribe(resp =>{
+
+    console.log('resp :>> ', resp);
+
+    },err => console.error(err))
+
+  }
+  //title = 'frontPersonal';
 }
