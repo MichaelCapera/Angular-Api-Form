60253633dff787c33759324bce3fd8fdeb415772
Michael Capera <michaelcapera@gmail.com>
2021-09-26 21:53:50 -0500

    create form validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonalService } from './services/personal.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent implements OnInit {
 
+  data:any;
+  bosses:any;
 
-  constructor(private personalService:PersonalService){}
-  
+  public f = this.form.group({
+    name:['',[Validators.required]],
+    type:['',[Validators.required]],
+    boss:['',[Validators.required]]
+
+  });
+
+  formSubmitted = false;
+
+  constructor(private personalService:PersonalService,
+              private form:FormBuilder){}
+
   ngOnInit(){
 
     this.personalService.getPersonal().subscribe(resp =>{
 
-    console.log('resp :>> ', resp);
+    this.data = Object.values(resp);  
 
-    },err => console.error(err))
+    },err => console.error(err));
 
-  }
+    this.personalService.getBosses().subscribe(resp =>{
+
+      this.bosses = Object.values(resp); 
+    
+    },err => console.error(err));
+
+    }
+    
   //title = 'frontPersonal';
+
+  savePerson(){
+
+    this.formSubmitted = true;
+    if(this.f.invalid){
+      return;
+    }
+    console.log('this.f :>> ', this.f);
+  }
+
+  invalidField(field:string){
+
+    if(this.formSubmitted && this.f.controls[field].invalid){
+
+      return true;
+
+    }else{
+
+      return false;
+
+    }
+  }
 }
